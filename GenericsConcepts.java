d49f07de0acd9a01c3f0242463357738031e213b
ExclusivePour <jay.jaganathan@outlook.com>
2025-01-12 18:09:29 +0530

    updated Generics

--- GenericsConcepts.java
+++ GenericsConcepts.java
@@ -1,2 +1,26 @@
 public class GenericsConcepts {
+
+    public static void main(String[] args){
+        GenericsConcepts gc = new GenericsConcepts();
+        gc.run();
+    }
+
+    public void run(){
+        Can<String> can = new Can<>("Worms");
+        String x = can.whatsInside();
+        System.out.println(x);
+    }
+
+    class Can<T>{
+
+        T contents;
+        public Can(T contents){
+            this.contents=contents;
+        }
+        public T whatsInside(){
+            return contents;
+        }
+
+    }
 }
+
